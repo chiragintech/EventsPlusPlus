5c49f8f6a3579c1ab8deb63aef7b1d0ac74e31ba
Chiragm550 <mchirag550@gmail.com>
2023-11-18 10:42:48 +0530

    added sort by category field

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,6 +7,7 @@ import axios from "axios";
 
 function Home() {
     let [events, setEvents] = useState([]);
+    let [eventType, setEventType] = useState("music");
 
     useEffect(() => {
         axios
@@ -21,6 +22,11 @@ function Home() {
             console.log(events);
         });
     }, [events]);
+
+    function handleEventType(type) {
+        setEventType(type);
+    }
+
     return (
         <div>
             <NavBar />
@@ -28,22 +34,43 @@ function Home() {
                 <h1 id="banner_desc">Plan Your Perfect Event With Event++</h1>
                 <p id="banner_span">It's all in the details</p>
             </div>
-            <h1 id="desc_header1">Latest</h1>
+            <h1 className="desc_header1">Latest</h1>
             <div className="event">
                 {events.map((event, i) => (
                     <Events
-                        display = {i>5?'none':'block'}
-                        name = {event.task.eventName}
+                        display={i > 6 ? "none" : "block"}
+                        name={event.task.eventName}
                         img={event.task.eventImgURL}
                         address={event.task.eventAddress}
                         date={event.task.eventDate}
                         about={event.task.eventAbout}
                         price={event.task.eventPrice}
-                        key = {i}
+                        key={i}
                     />
                 ))}
             </div>
-            <Footer/>
+            <h1 className="desc_header1">Categories</h1>
+            <div className="btn-row">
+                <button onClick={() => handleEventType("music")}>Music</button>
+                <button onClick={() => handleEventType("technical")}>Technical</button>
+                <button onClick={() => handleEventType("sports")}>Sports</button>
+                <button onClick={() => handleEventType("health")}>Health</button>
+            </div>
+            <div className="event">
+                {events.map((event, i) => (
+                    <Events
+                        display={event.task.eventType === eventType?'block':'none'}
+                        name={event.task.eventName}
+                        img={event.task.eventImgURL}
+                        address={event.task.eventAddress}
+                        date={event.task.eventDate}
+                        about={event.task.eventAbout}
+                        price={event.task.eventPrice}
+                        key={i}
+                    />
+                ))}
+            </div>
+            <Footer />
         </div>
     );
 }
